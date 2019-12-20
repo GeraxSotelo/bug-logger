b2cc87bfa99e169f6afe70e6925adb6e35c2f345
Gerax Sotelo <gsotelo23@gmail.com>
2019-12-20 14:41:53 -0700

    Add methods to bugs service

--- server/services/BugsService.js
+++ server/services/BugsService.js
@@ -16,6 +16,25 @@ class BugsService {
     }
     return data;
   }
+
+  async create(rawData) {
+    return await _repository.create(rawData);
+  }
+
+  async edit(id, update) {
+    let data = await _repository.findByIdAndUpdate({ _id: id }, update, { new: true });
+    if (!data) {
+      throw new ApiError("Invalid ID bro");
+    }
+    return data;
+  }
+
+  async delete(id) {
+    let data = await _repository.findByIdAndUpdate({ _id: id }, { closed: true });
+    if (!data) {
+      throw new ApiError("Invalid ID bro");
+    }
+  }
 }
 
 const bugsService = new BugsService();
