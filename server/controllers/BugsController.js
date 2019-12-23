3ac65a4f760fc06b8ed3d371811a9a07842d00a5
Gerax Sotelo <gsotelo23@gmail.com>
2019-12-23 15:23:00 -0700

    Deleted extra files

--- server/controllers/BugsController.js
+++ server/controllers/BugsController.js
@@ -34,7 +34,7 @@ export default class BugsController {
   async create(req, res, next) {
     try {
       let data = await BugsService.create(req.body);
-      return res.send(data);
+      return res.status(201).send(data);
     } catch (error) {
       next(error)
     }
