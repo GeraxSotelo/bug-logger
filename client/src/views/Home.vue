0d98aa1521a86004e8ef846aa154cc7f237def0c
Gerax Sotelo <gsotelo23@gmail.com>
2019-12-27 14:11:27 -0700

    Add styling to home page

--- client/src/views/Home.vue
+++ client/src/views/Home.vue
@@ -1,6 +1,33 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png" />
+  <div class="home container-fluid">
+    <!-- <img alt="Vue logo" src="../assets/logo.png" /> -->
+    <div class="row home-container">
+      <div class="col-12 pb-5">
+        <h1 class="mt-5">Bug-A-Boos Everywhere</h1>
+      </div>
+      <div class="col-12 pt-5">
+        <h3>Report Your Bug!</h3>
+        <router-link :to="{name:'bugs'}">
+          <button class="btn p-3 mt-3">
+            <h5>Go Now!</h5>
+          </button>
+        </router-link>
+      </div>
+    </div>
+    <div class="row pt-3">
+      <div class="col-12 card col-md-4">
+        <h2>BUG</h2>
+        <p>Report a Bug</p>
+      </div>
+      <div class="col-12 card col-md-4">
+        <h2>BUG</h2>
+        <p>We fix it</p>
+      </div>
+      <div class="col-12 card col-md-4">
+        <h2>BUG</h2>
+        <p>You're happy</p>
+      </div>
+    </div>
   </div>
 </template>
 
@@ -12,3 +39,21 @@ export default {
   components: {}
 };
 </script>
+
+<style>
+.btn {
+  color: orange;
+  background-color: white;
+  font-weight: bold;
+}
+.home-container {
+  background-color: orange;
+  color: white;
+  padding-top: 2em;
+  padding-bottom: 4.5em;
+}
+.card {
+  min-height: 10em;
+  justify-content: space-around;
+}
+</style>
